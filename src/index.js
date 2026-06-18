716992eaa7e51fe047398dd8b2263e05b6be34c0
gotakshn-808 <jahsiahkspalakiko@gmail.com>
2026-06-17 23:12:11 -0700

    Update index.jsx

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MechConnect from "./Apps.jsx";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <MechConnect />
   </React.StrictMode>
 );
